6d68521958a7bb3af16527831389112df78abad6
eric183 <kk297466058@gmail.com>
2023-05-18 17:07:09 +0800

    updated

---
+++ index.ts
@@ -0,0 +1,23 @@
+import { ISanityDocument } from "./typing";
+
+export const uploadSanity = async (file: File, url: string, Sanity_Token: string): Promise<ISanityDocument> => {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = async () => {
+      const arrayBuffer = reader.result;
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": file.type,
+          Authorization: `Bearer ${Sanity_Token}`,
+        },
+        body: arrayBuffer,
+      });
+      const { document } = await response.json();
+
+      resolve(document);
+    };
+  });
+};
